4f8ffb9d1a30b124f04487057f77655ac8764dd2
Your Name <you@example.com>
2021-05-12 17:07:15 +0800

    ss

---
+++ src/main/java/com/example/tagtest/TagTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.tagtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tagtest/TagTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tagtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
